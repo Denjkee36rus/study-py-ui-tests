b4c3db6912c1d5fce5e3332b8791b318003d2e41
Denis Chekalkin <chekalkin_92@mail.ru>
2024-12-11 14:15:06 +0300

    add_comment_test

--- tests/test_ft.py
+++ tests/test_ft.py
@@ -9,16 +9,20 @@ from pages.cart_page import CartPage
 @pytest.mark.usefixtures("browser")
 class TestFT:
     def test_add_product_to_cart(self, browser):
+        # Вбиваем в строку поиска, на главной страницы, искомый товар
         main_page = MainPage(browser)
         main_page.search_for_product('Гарри Поттер и философский камень')
 
+        # Закрываем попап скидок и выбираем искомый продукт
         search_results_page = SearchResultsPage(browser)
         search_results_page.close_discount_popup()
         search_results_page.click_on_product()
 
+        # Добавляем товар в карзину
         product_page = ProductPage(browser)
         product_page.add_to_cart()
 
+        # Проверяем наличие товара в карзине
         cart_page = CartPage(browser)
         cart_page.go_to_cart()
         time.sleep(4)  # Временная задержка для отладки
